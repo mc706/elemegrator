5d2777da6e2e34bf2f69cd9cfe9e7b980a11fae3
Ryan McDevitt <rmcdevitt@foxgrp.net>
2013-05-27 11:45:45 -0400

    added urls to match test feed

--- aggregator/feeds/urls.py
+++ aggregator/feeds/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('feeds.views',
     url(r'new/$', 'new_feed', name='new_feed'),
+    url(r'test/$', 'test_feed', name='test_feed'),
     url(r'all/$', 'feed_index', name='feed_index'),
     url(r'my/$', 'user_feeds', name='user_feeds'),
     url(r'sample/$', 'sample_user_feeds', name='sample_user_feeds'),
