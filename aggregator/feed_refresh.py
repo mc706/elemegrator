c4f70a695a40cf9e973b7f7574d2e94cba1c003e
Ryan McDevitt <rmcdevitt@foxgrp.net>
2013-05-29 01:25:02 -0400

    fixed source  problem?

--- aggregator/feed_refresh.py
+++ aggregator/feed_refresh.py
@@ -1,5 +1,6 @@
 from django.core.management import setup_environ
 from aggregator import settings
+import urllib2
 
 setup_environ(settings)
 
@@ -10,5 +11,7 @@ feeds = Feed.objects.filter(published=False)
 for feed in feeds:
     feed.published = True
     feed.save()
+
+urllib2.urlopen('http://webaggregator.net/feeds/sample/').read()
 
 
